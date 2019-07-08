7650086f64909790a24e76e8b59e1020ae75a891
Ubuntu <ubuntu@ip-172-31-85-149.ec2.internal>
2019-07-08 12:28:34 +0000

    Created addrecipes.html file with functionaltiy via app.py to add recipies to the database. Added 'New Recipe' to navbar.

--- app.py
+++ app.py
@@ -12,15 +12,37 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:537743root@jesscluster-o9ja6.mongo
 
 mongo = PyMongo(app)
 
+# Get Recipies 
+
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", 
                            recipes=mongo.db.recipes.find())
                            
+# Add Recipe 
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+                           recipes=mongo.db.recipes.find())
                            
+
+# Insert Recipe and redirect
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+  
+  
+  
+  
+
+  
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
