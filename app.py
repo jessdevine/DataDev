5f7e337160eea5caeff730c1a710f9cd39b2eb65
Ubuntu <ubuntu@ip-172-31-85-149.ec2.internal>
2019-07-08 09:48:22 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+app = Flask(__name__)
+
+#Connecting to MongoDB "Online Cookbook"
+
+app.config["MONGO_DBNAME"] = 'cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://root:537743root@jesscluster-o9ja6.mongodb.net/cookbook?retryWrites=true'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+                           recipes=mongo.db.recipes.find())
+                           
+                           
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
